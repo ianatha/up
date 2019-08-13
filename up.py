93fe8b1602ad0b4f8eef0bf47303eee2326bf1a2
Ian Atha <ian@atha.io>
2019-08-13 11:12:52 -0700

    Support interactive usage when no arg is given

--- up.py
+++ up.py
@@ -3,19 +3,32 @@
 from __future__ import print_function
 import os, sys
 
-if len(sys.argv) == 2:
-    if sys.argv[1].isdigit():
-        levels_up = int(sys.argv[1])
-        print("../" * levels_up)
-    else:
-        match = sys.argv[1]
-        cwd = os.getcwd().split(os.sep)[::-1][:-1]
-        try:
-            levels_up = cwd.index(match)
-            print("../" * levels_up)
-        except:
-            print("up: '%s' is neither a number, nor something in your path" % match, file=sys.stderr)
-            print("__no_path_found__")    
+cwd = os.getcwd().split(os.sep)[::-1][:-1]
+
+arg_input = None
+if len(sys.argv) != 2:
+    for i, dir in enumerate(cwd):
+        print("% 2d. %s" % (i, dir), file=sys.stderr)
+    print("? ", file=sys.stderr, end='')
+    arg_input = raw_input()
 else:
-    print("up: up [levels_up|parent_dir]", file=sys.stderr)
-    print("__no_path_found__")    
+    arg_input = sys.argv[1]
+
+levels_up = None
+
+if arg_input.isdigit():
+    levels_up = int(arg_input)
+elif arg_input == '':
+    levels_up = 0
+else:
+    match = arg_input
+    try:
+        levels_up = cwd.index(match)
+    except:
+        print("up: '%s' is neither a number, nor something in your path" % match, file=sys.stderr)
+        print("__no_path_found__")
+
+if levels_up > 0:
+    print("../" * levels_up)
+else:
+    print(".")
