38e38f571c144f34471efc5267c59c0f35b776bc
Ian Atha <ian@atha.io>
2019-08-13 11:22:12 -0700

    Add support for '~' as arg

--- up.py
+++ up.py
@@ -2,8 +2,10 @@
 
 from __future__ import print_function
 import os, sys
+from os.path import expanduser
 
 cwd = os.getcwd().split(os.sep)[::-1][:-1]
+home = expanduser("~")
 
 arg_input = None
 if len(sys.argv) != 2:
@@ -18,6 +20,8 @@ levels_up = None
 
 if arg_input.isdigit():
     levels_up = int(arg_input)
+elif arg_input == '~' or arg_input == home:
+    levels_up = -1
 elif arg_input == '':
     levels_up = 0
 else:
@@ -30,5 +34,7 @@ else:
 
 if levels_up > 0:
     print("../" * levels_up)
+elif levels_up == -1:
+    print("")
 else:
     print(".")
